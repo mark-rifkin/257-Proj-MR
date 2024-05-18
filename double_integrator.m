4cfeff1dcc7b013a2ca695fe2818c17eceaf8c45
Mark <mrifkin2022@francisparker.org>
2024-05-18 02:11:24 -0400

    vectorization, todo better monomial substitution

--- double_integrator.m
+++ double_integrator.m
@@ -58,23 +58,43 @@ X = [X1(:), X2(:)];
 Nx = size(X, 1);
 
 % samples of u
-Nu = 101;
+Nu = 100;
 u_vals = linspace(-1, 1, Nu);
 
 % select u_out from samples of u for test x 
 u_out = zeros(1, Nx);
-for i = 1:Nx
-    b_eval = double(subs(b, {x(1), x(2), u}, {X(i, 1), X(i, 2), u_vals})); % evaluate monomial basis at selected x and all u
 
-    q_eval = diag(b_eval.'*Psq*b_eval); % 1 by Nu -- evaluates q(x, u) for each u and given x
-    min_idx = find(q_eval == min(q_eval)); % indices of min(q(x, u))
-    u_out(i) = min(u_vals(min_idx)); % min(argmin u q(x,u)) -- outer minimization breaks ties
-    
-end
+
+% Original non-vectorized for x samples (it's not actually slower because subs is slow) 
+% for i = 1:Nx
+%     b_eval = double(subs(b, {x(1), x(2), u}, {X(i, 1), X(i, 2), u_vals})); % evaluate monomial basis at selected x and all u
+% 
+%     q_eval = sum(b_eval'*Psq.*b_eval', 2); % 1 by Nu -- evaluates q(x, u) for each u and given x
+%     [~, idx] = min(q_eval); % this will also take outer min if u is sorted in ascending order
+%     u_out(i) = u_vals(idx);
+% 
+% end
+
+
+[X2, U, X1] = meshgrid(linspace(-1, 1, npoints), u_vals, linspace(-1, 1, npoints));
+X1 = X1(:); X2 = X2(:); U = U(:);
+
+tic; 
+b_eval_et = double(subs(b, {x(1), x(2), u}, {X1', X2', U'})); % outputs b by nu*nx matrix
+t1 = toc; tic;
+q_eval_et = sum(b_eval_et'*Psq.*b_eval_et', 2); % dot product -> sum to avoid multiplying large matrix
+t2 = toc; tic;
+q_eval_et_res = reshape(q_eval_et, Nu, Nx)'; % nx by nu matrix where rows compare u for a given x
+t3 = toc; tic;
+[~, idx] = min(q_eval_et_res, [], 2); % this will also take outer min if u is sorted in ascending order
+u_out_2 = u_vals(idx);
+t4 = toc;
 
 u_are_vec = clip(-K_are*X', -1, 1);
 u_err = abs(u_are_vec - u_out);
-u_err > 2/Nu % this is all 0 so u_out is always closest possible to u from ARE
+u_err > 2/Nu; % this is all 0 so u_out is always closest possible to u from ARE
+
+%%
 
 %% SOSTools (to check YALMIP)
 clc; clear; close all;
